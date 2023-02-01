01399d17a77830db9ed6042c3f56412ce4d8700b
vladimir <vladimiramnell@gmail.com>
2023-02-01 12:18:48 +0300

    add kitty

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,7 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
-	adapter "github.com/unheilbar/artforintrovert_entry_task/internal/adapters"
-	"github.com/unheilbar/artforintrovert_entry_task/internal/api"
-	"github.com/unheilbar/artforintrovert_entry_task/internal/config"
-	"github.com/unheilbar/artforintrovert_entry_task/internal/scheduler"
-	"github.com/unheilbar/artforintrovert_entry_task/internal/server"
-	"github.com/unheilbar/artforintrovert_entry_task/internal/service/user"
-	"github.com/unheilbar/artforintrovert_entry_task/internal/storage"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // TODO
@@ -24,45 +10,5 @@ import (
 // bd connection
 
 func main() {
-	config, err := config.LoadConfig(".")
-	if err != nil {
-		panic(err)
-	}
-
-	opts := options.Client().ApplyURI(config.DSN)
-	mongoClient, err := mongo.Connect(context.Background(), opts)
-	if err != nil {
-		panic(err)
-	}
-
-	err = mongoClient.Ping(context.TODO(), nil)
-	if err != nil {
-		panic(err)
-	}
-
-	provider := storage.NewStorageProvider(mongoClient, config.DbName, config.CollectionName)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	go scheduler.RunRefreshJob(ctx, provider.RefreshCache, config.CacheRefreshInterval)
-
-	storageProvider := adapter.NewStorageProviderAdapter(provider)
-
-	service := user.NewService(storageProvider)
-
-	handlers := api.NewHandlers(service)
-
-	server := server.NewServer(handlers.GetRouter(), config.ServerAddress)
-
-	serverErrChan := server.Start()
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-	select {
-	case <-signals:
-		fmt.Println("main: got terminate signal. Shutting down...", nil)
-		cancel()
-	case <-serverErrChan:
-		fmt.Println("main: got server err signal. Shutting down...", nil)
-	}
+	fmt.Println("")
 }
--- internal/entities/user.go
+++
@@ -1,7 +0,0 @@
-package entities
-
-type User struct {
-	ID       string `json:"id" bson:"id,omitempty"`
-	Nickname string `json:"nickname" bson:"nickname,omitempty"`
-	Age      uint   `json:"age" bson:"age,omitempty"`
-}
